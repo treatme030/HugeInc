f801dff357b1ae8f92efc96de7f16fa08bb0793f
treatme030 <handam81@naver.com>
2021-04-14 15:55:16 +0900

    수정

--- js/contact.js
+++ js/contact.js
@@ -12,25 +12,60 @@ function titleChange(){
     document.getElementById('hero_title').innerHTML = titleText[titleTextNum];
 }
 
+//input요소 이벤트
 
-//Talk form focus/blur event
 let talkItem = document.getElementsByClassName('talk-item');
-let talkEyebrow = document.getElementsByClassName('talk_eyeborw');
 
 for(let item of talkItem){
+
+    //input focus/blur event
     item.addEventListener('focus', function(){
-        for(let eyebrow of talkEyebrow){
-            if(eyebrow.getAttribute('for') === this.getAttribute('id')){
-                eyebrow.style.display = 'block';
-            }
+        this.parentNode.classList.add('active');
+    });
+    item.addEventListener('blur', function(){
+        if(this.value.length == 0){//입력된 값이 없을 때
+            this.parentNode.classList.remove('active');
+        } else {
+            this.parentNode.classList.add('active_2');//입력된 값이 있을 때
         }
     });
 
-    item.addEventListener('blur', function(){
-        for(let eyebrow of talkEyebrow){
-            if(eyebrow.getAttribute('for') === this.getAttribute('id')){
-                eyebrow.style.display = 'none';
-            }
+    //값이 입력될 때 input event
+    item.addEventListener('input', function(){
+        if(this.value.length > 1){//입력 값이 두글자 이상일 때
+            this.parentNode.classList.remove('error');
+        } else {
+            this.parentNode.classList.add('error');
         }
     });
 }
+
+//Sendmessage button click event
+document.getElementById('talk_send').addEventListener('click', function(e){
+    e.preventDefault();
+    let isCheck = true;
+    
+    if(talkItem[0].value == ''){
+        talkItem[0].parentNode.classList.add('error');
+        isCheck = false;
+    }
+    if(talkItem[1].value == ''){
+        talkItem[1].parentNode.classList.add('error');
+        isCheck = false;
+    }
+    if(talkItem[2].value == ''){
+        talkItem[2].parentNode.classList.add('error');
+        isCheck = false;
+    }
+    if(talkItem[3].value == ''){
+        talkItem[3].parentNode.classList.add('error');
+        isCheck = false;
+    }
+    if(isCheck){
+        document.getElementById('talkRight').style.display = 'none';
+        document.getElementById('success').style.display = 'block';
+    }
+    isCheck = true; 
+});
+
+
