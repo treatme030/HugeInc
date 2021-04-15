de4b4268e34f55be44b897bfb7c3746717262d9f
treatme030 <74355328+treatme030@users.noreply.github.com>
2021-04-15 17:48:48 +0900

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -63,6 +63,26 @@ document.getElementById('talk_send').addEventListener('click', function(e){
 let presentTime = new Date(),
     utcHours = presentTime.getUTCHours(), //UTC 시간 구하기
     utcMinutes = presentTime.getUTCMinutes();//UTC 분 구하기
+    if(utcMinutes < 10){
+        utcMinutes = `0${utcMinutes}`;
+    } 
+// let cityHour = [];
+// let cityHourTimeOffset = [-4,-5,,-4,-5,-4,-4,0,-7,-5,-7,8,-5,9];
+// let cityAmPm = [];
+
+// for(let i = 0; i < 13 ; i++){
+//     //24시간제
+//     cityHour[i] = utcHours + cityHourTimeOffset[i];
+//     if(cityHour[i] < 0){
+//         cityHour[i] += 24;
+//     }
+//     //ampm 변경하기
+//     if(cityHour[i] >= 12){
+//         cityHour[i] = 'PM';
+//     } else {
+//         cityHour[i] = 'AM';
+//     }
+// }
 
 //각 도시 UTC
 let atlanta = utcHours - 5 + 1,
@@ -71,7 +91,7 @@ let atlanta = utcHours - 5 + 1,
     chicago = utcHours - 5,
     dc = utcHours - 5 + 1,
     detroit = utcHours - 5 + 1,
-    london =utcHours + 1,
+    london =utcHours,
     losAngeles =utcHours - 8 + 1,
     medellin = utcHours - 5,
     oakland =utcHours - 8 + 1,
@@ -87,30 +107,39 @@ let officeBox = document.getElementsByClassName('office-box'),
 
 //각 officeBox마다 할 일
 officeBoxArray.forEach((box, index) => {
-
-    time(index);
+    //24시간제
+    if(timeArray[index] < 0){
+        timeArray[index] += 24;
+    }
+    //ampm 구하기
+    if(timeArray[index] >= 0 && timeArray[index] < 12){
+        AmPm = 'AM';
+    } else if(timeArray[index] >= 12 && timeArray[index] < 24 ){
+        AmPm = 'PM';
+    }
     
     if(timeArray[index] >= 6 && timeArray[index] <= 18){//낮인 경우
         officeBoxArray[index].classList.remove('night');
         officeBoxArray[index].classList.add('day');
-        hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
     } else {//밤인 경우
         officeBoxArray[index].classList.remove('day');
         officeBoxArray[index].classList.add('night');
-        hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
     }
+    //12시간제로 변경
+    if (timeArray[index] > 12) { timeArray[index] -= 12; }
+
+    hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
+    hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
 });
-//각 도시 현재시간 나타내는 함수
-function time(t){
-    if(timeArray[t] < 0){
-        timeArray[t] += 24;
-    }
-    if(timeArray[t] >= 24){
-        timeArray[t] -= 24;
-    }
-    if(timeArray[t] >= 0 && timeArray[t] < 12){
-        AmPm = 'AM';
-    } else if(timeArray[t] >= 12 && timeArray[t] < 24 ){
-        AmPm = 'PM';
-    } 
-}
+
+//리셋될 경우 latest-news 배경이미지 변경
+let magentaBg = ['m1.jepg','m2.jpg','m3.jpg'];
+let magentaTitle = ["Predictions for the Year Ahead" ,"Should Logo Designers Fear the Internet","Fighting for Progress."];
+let magentaDes = ['Huge strategists, technologists, UX designers, and creatives weigh in on what’s really relevant in 2019.', 'How the New York Civil Liberties Union and Huge harnessed the power of retro technology to publicize the limits of arcane legislation.','merong']
+
+let randomNum = Math.floor(Math.random()*magentaBg.length);
+document.getElementsByClassName('latest-news')[0].style.backgroundImage = `'url(../${magentaBg[randomNum]}'`;
+
+document.getElementsByClassName('title')[0].innerHTML = magentaTitle[randomNum];
+document.getElementsByClassName('dese')[0].innerHTML = magentaDes[randomNum];
+
