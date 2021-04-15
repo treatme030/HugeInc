e4d79640a89a89d126f237ef66509787141dda95
treatme030 <handam81@naver.com>
2021-04-15 09:36:04 +0900

    send 버튼 클릭 이벤트 수정/ 도시 시간 함수 수정

--- js/contact.js
+++ js/contact.js
@@ -7,14 +7,15 @@ var titleI = setInterval (titleChange, 1000);
 function titleChange(){
     titleTextNum++;
     if(titleTextNum == titleText.length){
-            titleTextNum =0;
-        }
+        titleTextNum =0;
+    }
     document.getElementById('hero_title').innerHTML = titleText[titleTextNum];
 }
 
 //input요소 이벤트
 
 let talkItem = document.getElementsByClassName('talk-item');
+let talkItemArray = [...talkItem];
 
 for(let item of talkItem){
 
@@ -32,7 +33,7 @@ for(let item of talkItem){
 
     //값이 입력될 때 input event
     item.addEventListener('input', function(){
-        if(this.value.length > 1){//입력 값이 두글자 이상일 때
+        if(this.value.length > 1){//입력 값이 두글자 이상일 때 적용
             this.parentNode.classList.remove('error');
         } else {
             this.parentNode.classList.add('error');
@@ -43,81 +44,73 @@ for(let item of talkItem){
 //Sendmessage button click event
 document.getElementById('talk_send').addEventListener('click', function(e){
     e.preventDefault();
-    let isCheck = true;
-    
-    if(talkItem[0].value == ''){
-        talkItem[0].parentNode.classList.add('error');
-        isCheck = false;
-    }
-    if(talkItem[1].value == ''){
-        talkItem[1].parentNode.classList.add('error');
-        isCheck = false;
-    }
-    if(talkItem[2].value == ''){
-        talkItem[2].parentNode.classList.add('error');
-        isCheck = false;
-    }
-    if(talkItem[3].value == ''){
-        talkItem[3].parentNode.classList.add('error');
-        isCheck = false;
-    }
-    if(isCheck){
+    //입력 값이 입력되지 않은 경우
+    talkItemArray.forEach((item, index) => {
+        if(talkItem[index].value == ''){
+            talkItem[index].parentNode.classList.add('error');
+        }
+    });
+
+    //입력 값이 모두 입력된 경우 성공메세지 출력
+    if(document.getElementsByClassName('error').length == 0){
         document.getElementById('talkRight').style.display = 'none';
         document.getElementById('success').style.display = 'block';
     }
-    isCheck = true; 
 });
 
+//각 도시 현재 시간 나타내기
 
-//각 도시 현재 시간
-let presentTime = new Date();
-let utcHours = presentTime.getUTCHours();
-let utcMinutes = presentTime.getUTCMinutes();
+let presentTime = new Date(),
+    utcHours = presentTime.getUTCHours(), //UTC 시간 구하기
+    utcMinutes = presentTime.getUTCMinutes();//UTC 분 구하기
 
 //각 도시 UTC
-let atlanta = utcHours - 5 + 1;
-let bogota = utcHours - 5;
-let brooklyn = utcHours - 5 + 1;
-let chicago = utcHours - 5;
-let dc = utcHours - 5 + 1;
-let detroit = utcHours - 5 + 1;
-let london =utcHours + 1;
-let losAngeles =utcHours - 8 + 1;
-let medellin = utcHours - 5;
-let oakland =utcHours - 8 + 1;
-let singapore =utcHours + 8;
-let toronto = utcHours - 5 + 1;
-let tokyo = utcHours + 9;
-let timeArray = [atlanta,bogota,brooklyn,chicago,dc,detroit,london,losAngeles,medellin,oakland,singapore,toronto,tokyo];
-let AmPm;
+let atlanta = utcHours - 5 + 1,
+    bogota = utcHours - 5,
+    brooklyn = utcHours - 5 + 1,
+    chicago = utcHours - 5,
+    dc = utcHours - 5 + 1,
+    detroit = utcHours - 5 + 1,
+    london =utcHours + 1,
+    losAngeles =utcHours - 8 + 1,
+    medellin = utcHours - 5,
+    oakland =utcHours - 8 + 1,
+    singapore =utcHours + 8,
+    toronto = utcHours - 5 + 1,
+    tokyo = utcHours + 9,
+    timeArray = [atlanta,bogota,brooklyn,chicago,dc,detroit,london,losAngeles,medellin,oakland,singapore,toronto,tokyo],
+    AmPm;
 
-function time(t){
-        if(timeArray[t] < 0){
-            timeArray[t] += 24;
-        }
-        if(timeArray[t] >= 0 && timeArray[t] < 12){
-            AmPm = 'AM';
-        } else if(timeArray[t] >= 12 && timeArray[t] < 24 ){
-            AmPm = 'PM';
-        } 
-}
+let officeBox = document.getElementsByClassName('office-box'),
+    hours = document.getElementsByClassName('hours'),
+    officeBoxArray = [...officeBox];
 
-let officeBox = document.getElementsByClassName('office-box');
-let hours = document.getElementsByClassName('hours');
-let officeBoxArray = [...officeBox];
-console.log(officeBoxArray);
-
+//각 officeBox마다 할 일
 officeBoxArray.forEach((box, index) => {
 
     time(index);
-
-    if(timeArray[index] >= 6 && timeArray[index] <= 18){
+    
+    if(timeArray[index] >= 6 && timeArray[index] <= 18){//낮인 경우
         officeBoxArray[index].classList.remove('night');
         officeBoxArray[index].classList.add('day');
         hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
-    } else {
+    } else {//밤인 경우
         officeBoxArray[index].classList.remove('day');
         officeBoxArray[index].classList.add('night');
         hours[index].innerHTML = `${timeArray[index]}:${utcMinutes} ${AmPm}`;
     }
 });
+//각 도시 현재시간 나타내는 함수
+function time(t){
+    if(timeArray[t] < 0){
+        timeArray[t] += 24;
+    }
+    if(timeArray[t] >= 24){
+        timeArray[t] -= 24;
+    }
+    if(timeArray[t] >= 0 && timeArray[t] < 12){
+        AmPm = 'AM';
+    } else if(timeArray[t] >= 12 && timeArray[t] < 24 ){
+        AmPm = 'PM';
+    } 
+}
